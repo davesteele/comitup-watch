f6af962ab20f6714953e9464f27792ce341ec282
David Steele <steele@debian.org>
2021-07-04 11:42:09 -0400

    Make avahi_watch asyncio-compatible

--- comitup_watch/avahi_watch.py
+++ comitup_watch/avahi_watch.py
@@ -1,30 +1,89 @@
-
+import asyncio
+import re
+from enum import Enum
+from typing import List, NamedTuple, Optional
 
 from zeroconf import Zeroconf, ServiceBrowser
 
+
+class AvahiAction(Enum):
+    ADDED = "ADDED"
+    REMOVED = "REMOVED"
+
+
+class AvahiMessage(NamedTuple):
+    action: AvahiAction
+    key: str
+    name: str
+    host: str
+    ipv4: str
+    ipv6: str
+
+
 class MyListener:
-    def __init__(self, zc):
+    def __init__(self, zc, loop, q):
         self.zc = zc
+        self.loop = loop
+        self.q = q
 
     def remove_service(self, zeroconf, tipe, name):
-        print("Removed - {}".format(name))
+        msg = AvahiMessage(
+            AvahiAction.REMOVED,
+            name,
+            None,
+            None,
+            None,
+            None,
+        )
+        asyncio.run_coroutine_threadsafe(self.q.put(msg), self.loop)
+
+    def get_ipv4(self, addrlist: List[str]) -> Optional[str]:
+        for candidate in addrlist:
+            if re.search("^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$", candidate):
+                return candidate
+        return None
+
+    def get_ipv6(self, addrlist: List[str]) -> Optional[str]:
+        for candidate in addrlist:
+            if re.search("^[0-9a-f:]+$", candidate):
+                return candidate
+        return None
 
     def add_service(self, zeroconf, tipe, name):
-        print("Added - {}".format(name))
+        si = self.zc.get_service_info("_comitup._tcp.local.", name)
 
-        si = zc.get_service_info("_comitup._tcp.local.", name)
-
-        print("    {}".format(si.parsed_addresses()))
-        print("    {}".format(si.get_name()))
+        msg = AvahiMessage(
+            AvahiAction.ADDED,
+            name,
+            si.get_name(),
+            si.properties[b"hostname"].decode(),
+            self.get_ipv4(si.parsed_addresses()),
+            self.get_ipv6(si.parsed_addresses()),
+        )
+        asyncio.run_coroutine_threadsafe(self.q.put(msg), self.loop)
 
     def update_service(self, *args, **kwargs):
         pass
 
-zc = Zeroconf()
-listener = MyListener(zc)
-browser = ServiceBrowser(zc, "_comitup._tcp.local.", listener)
 
-try:
-    input("")
-finally:
-    zeroconf.close()
+async def amain():
+    loop = asyncio.get_event_loop()
+
+    avahi_q = asyncio.Queue()
+
+    # loop.run_in_executor(None, monitor_avahi, loop)
+    zc = Zeroconf()
+    listener = MyListener(zc, loop, avahi_q)
+    browser = ServiceBrowser(zc, "_comitup._tcp.local.", listener)
+
+    while True:
+        msg = await avahi_q.get()
+        print(msg)
+
+
+def main():
+    asyncio.run(amain())
+
+
+if __name__ == "__main__":
+    main()
