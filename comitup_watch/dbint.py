3a45e54561af76642dd4255b725f75e4b67631e8
David Steele <steele@debian.org>
2021-07-04 11:41:33 -0400

    Move DBInt to its own file

---
+++ comitup_watch/dbint.py
@@ -0,0 +1,36 @@
+import asyncio
+import re
+from datetime import datetime
+from typing import List, Set
+
+import dbussy
+import ravel
+
+
+class DBInt:
+    _cache = {}
+    bus = None
+
+    def __init__(self, bus):
+        DBInt.bus = bus
+
+    @staticmethod
+    async def get_interface(busname, path, interface):
+        if (busname, path, interface) not in DBInt._cache:
+            intfc = await DBInt.bus[busname][path].get_async_interface(
+                interface
+            )
+            DBInt._cache[(busname, path, interface)] = intfc
+
+        return DBInt._cache[(busname, path, interface)]
+
+    @staticmethod
+    async def GetAllDevices():
+        intfc = await DBInt.get_interface(
+            "org.freedesktop.NetworkManager",
+            "/org/freedesktop/NetworkManager",
+            "org.freedesktop.NetworkManager",
+        )
+
+        result = await intfc.GetAllDevices()
+        return result[0]
--- comitup_watch/devicemon.py
+++ comitup_watch/devicemon.py
@@ -6,34 +6,7 @@ from typing import List, Set
 import dbussy
 import ravel
 
-
-class DBInt:
-    _cache = {}
-    bus = None
-
-    def __init__(self, bus):
-        DBInt.bus = bus
-
-    @staticmethod
-    async def get_interface(busname, path, interface):
-        if (busname, path, interface) not in DBInt._cache:
-            intfc = await DBInt.bus[busname][path].get_async_interface(
-                interface
-            )
-            DBInt._cache[(busname, path, interface)] = intfc
-
-        return DBInt._cache[(busname, path, interface)]
-
-    @staticmethod
-    async def GetAllDevices():
-        intfc = await DBInt.get_interface(
-            "org.freedesktop.NetworkManager",
-            "/org/freedesktop/NetworkManager",
-            "org.freedesktop.NetworkManager",
-        )
-
-        result = await intfc.GetAllDevices()
-        return result[0]
+from .dbint import DBInt
 
 
 class DeviceMonitor(DBInt):
@@ -170,16 +143,16 @@ class APManager(DBInt):
                 )[0][1]
                 ssid = bytearray(ssid).decode()
 
-                if not ssid:
-                    await asyncio.sleep(1)
-                    ssid = (
-                        await intfc.Get(
-                            "org.freedesktop.NetworkManager.AccessPoint",
-                            "Ssid",
-                        )
-                    )[0][1]
-                    ssid = bytearray(ssid).decode()
-
+#                 if not ssid:
+#                     await asyncio.sleep(1)
+#                     ssid = (
+#                         await intfc.Get(
+#                             "org.freedesktop.NetworkManager.AccessPoint",
+#                             "Ssid",
+#                         )
+#                     )[0][1]
+#                     ssid = bytearray(ssid).decode()
+# 
                 if ssid:
                     ssids |= set([ssid])
             except dbussy.DBusError:
